e2e9f700be9cb5c168392fafb6ea7d0542e2a9bb
Jonas <40244574+JonasThers@users.noreply.github.com>
2022-02-16 20:29:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+require("dotenv").config();
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|css|eot|ttf)$/,
+        loader: "url-loader",
+      },
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              plugins: ["@babel/plugin-transform-runtime"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        envVar: JSON.stringify(process.env),
+      },
+    }),
+  ],
+};
